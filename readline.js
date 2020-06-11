b274d5660d4f652057605add35e4ef8c565faca6
Mikhail <mpodanev@gmail.com>
2020-06-11 11:45:56 +0300

    add testing dev environment (#15)
    * add testing dev environment
    
    * Update readline.js
    
    Co-authored-by: Roman Diachenko <dyaroman@gmail.com>
    
    * refactoring
    
    * Update utils.js
    
    Co-authored-by: Roman Diachenko <dyaroman@gmail.com>
    
    * Update utils.js
    
    Co-authored-by: Roman Diachenko <dyaroman@gmail.com>
    
    Co-authored-by: Roman Diachenko <dyaroman@gmail.com>

--- readline.js
+++ readline.js
@@ -1,28 +1,45 @@
 const fs = require('fs');
-const { AutoComplete, Select } = require('enquirer');
-const {launchBackstop, projectsDirectoryPath, checkLocalHost} = require('./utils');
+const {AutoComplete, Select, Input} = require('enquirer');
+const {launchBackstopLocal, launchBackstopDev, projectsDirectoryPath, checkLocalHost} = require('./utils');
 
-const select = new Select({
-    name: 'workflow',
-    message: 'Choose workflow',
-    choices: ['test', 'approve']
+const workflow = new Select({
+  name: 'workflow',
+  message: 'Choose workflow',
+  choices: ['test', 'approve']
 });
 
-const autoComplete = new AutoComplete({
-    name: 'projectName',
-    message: 'Choose your project',
-    limit: 10,
-    initial: 2,
-    choices: fs.readdirSync(projectsDirectoryPath)
+const env = new Select({
+  name: 'env',
+  message: 'Choose environment',
+  choices: ['local', 'dev']
+});
+
+const projectUrl = new Input({
+  message: 'Enter project url: ',
+  initial: 'bad-credit-loans.co'
+});
+
+const autoCompleteProject = new AutoComplete({
+  name: 'projectName',
+  message: 'Choose your project',
+  limit: 10,
+  initial: 2,
+  choices: fs.readdirSync(projectsDirectoryPath)
 });
 
 async function run() {
   try {
-    const commandToRun = await select.run();
-    const projectName = await autoComplete.run();
-    const availableLocalHost = await checkLocalHost(projectName);
-    if (availableLocalHost) {
-      launchBackstop(commandToRun, projectName);
+    const commandToRun = await workflow.run();
+    const environment = await env.run();
+    if (environment === 'dev') {
+      const projectName = await projectUrl.run();
+      launchBackstopDev(commandToRun, projectName);
+    } else {
+      const projectName = await autoCompleteProject.run();
+      const availableLocalHost = await checkLocalHost(projectName);
+      if (availableLocalHost) {
+        launchBackstopLocal(commandToRun, projectName);
+      }
     }
   } catch (e) {
     console.log(e);
